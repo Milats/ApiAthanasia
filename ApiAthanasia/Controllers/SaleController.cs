211cb25f7486e5597847effbc9dace046d681780
Stalin Moposita <organic38@hotmail.es>
2021-11-22 15:48:40 -0500

    Get Product&Sale Works

--- ApiAthanasia/Controllers/SaleController.cs
+++ ApiAthanasia/Controllers/SaleController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using ApiAthanasia.Models;
+using ApiAthanasia.Models.Response;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ApiAthanasia.Controllers
@@ -7,6 +9,26 @@ namespace ApiAthanasia.Controllers
     [ApiController]
     public class SaleController : ControllerBase
     {
-
+        [HttpGet]
+        public IActionResult Get()
+        {
+            Response R = new Response();
+            try
+            {
+                using (AthanasiaContext DB = new AthanasiaContext())
+                {
+                    var saleList = DB.Sales.OrderByDescending(b => b.Date)
+                        .ToList();
+                    R.Success = true;
+                    R.Message = "SaleGet Succesful";
+                    R.Data = saleList;
+                }
+            }
+            catch (Exception ex)
+            {
+                R.Message = ex.Message;
+            }
+            return Ok(R);
+        }
     }
 }
--- ApiAthanasia/Models/Response/Response.cs
+++ ApiAthanasia/Models/Response/Response.cs
@@ -5,7 +5,6 @@
         public bool Success { get; set; }
         public string Message { get; set; }
         public object Data { get; set; }
-
         public Response(){
             this.Success = false;
         }
